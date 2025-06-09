ca2758d18a8389fb2660b972b7dbd18d9d819a1f
escandon100 <olakunle23odunade@gmail.com>
2025-06-09 22:16:56 +0100

    earnings popup and minor changes

--- client/src/components/homepage/homepage.jsx
+++ client/src/components/homepage/homepage.jsx
@@ -8,6 +8,7 @@ import TradingSteps from '../tradingSteps/tradingSteps'
 import Download from '../download/download'
 import MainPage from '../mainPage/mainPage'
 import Chart from '../chart/chart'
+import EarningsPopup from '../earningsPopup/earningsPopup'
 
 const Homepage = () => {
   return (
@@ -55,6 +56,7 @@ const Homepage = () => {
           ></iframe>
         </div>
       </div>
+       <EarningsPopup/>
        <MainPage/>
         <Reason1/>
         <Plans/>
