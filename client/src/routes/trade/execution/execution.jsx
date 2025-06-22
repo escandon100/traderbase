1679dca640cefb247b74008ad467323715692f5e
escandon100 <olakunle23odunade@gmail.com>
2025-06-22 18:34:44 +0100

    complete trade section

--- client/src/routes/trade/execution/execution.jsx
+++ client/src/routes/trade/execution/execution.jsx
@@ -1,11 +1,15 @@
 import React from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHome } from "@fortawesome/free-solid-svg-icons";
 import { Link } from 'react-router';
+import "./execution.scss"
 
 const Execution= () =>{
     return ( 
      <>
         <div className="executionNav">
-
+            <Link className="link" to="/"><FontAwesomeIcon className="icon" icon={faHome} color="#4a5566"/><p>Main</p></Link><span>/ Payment methods</span>
+                  
         </div>
         <div className="execution">
             <div className="main">
@@ -13,10 +17,10 @@ const Execution= () =>{
                 <p>eqtradesendeavors to give merchants quick execution of requests with costs that are not in a general sense not quite the same as those expressed.
                     Execution of requests is completed in two different ways:</p>
 
-                    <ul>
+                    <ol>
                         <li>Promptly – the exchange opens right when the financial specialist has formalized his application.</li>
                         <li>At the point when the correct conditions arrive – the investor indicates the conditions (for instance, achieving a cost of a specific dimension) at the beginning of which execution happens.</li>
-                    </ul>
+                    </ol>
 
                     <p>Clients can oversee orders, indicating every one of their parameters, and furthermore making changes to them during the time spent work.</p>
                     <p>It would be ideal if you note that the request execution costs at any time might be somewhat not the same as those expressed. This is because of the unpredictability and liquidity of the market. We endeavor to guarantee the best execution of the exchange, picking the best cost available on the market!</p>
